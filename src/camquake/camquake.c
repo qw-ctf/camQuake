204ea96aa826514bb5e7cc8b0a379d84163e9cfb
Jürgen Legler <jogihoogi@gmail.com>
2020-10-04 14:27:07 +0200

    fix "camquake play"

--- src/camquake/camquake.c
+++ src/camquake/camquake.c
@@ -186,11 +186,8 @@ void Camquake_Frame(void)
 	// used by "camquake play"
 	if (camquake->active_setup) {
 		if (camquake->current_time <= camquake->active_setup->time_stop - camquake->active_setup->time_start) {
-			t = camquake->current_time + camquake->active_setup->time_start;
+			t = camquake->current_time;
 			CQS_Interpolate(camquake->active_setup, t, (struct camquake_path_point *)&r_refdef.vieworg, (struct camquake_path_point *)&r_refdef.viewangles);
-			if (t >= 1.0) {
-				camquake->active_setup = NULL;
-			}
 		} else {
 			camquake->active_setup = NULL;
 		}
